e65542b68a9e010ab600804002cf879cba44e0d7
Sunshine <sunshine.summerville@gmail.com>
2021-07-21 14:15:52 +0200

    test commit

---
+++ src/test/java/nl/novi/summerville/springboot/example/security/controller/TestReservationController.java
@@ -0,0 +1,33 @@
+package nl.novi.summerville.springboot.example.security.controller;
+
+import nl.novi.summerville.springboot.example.security.domain.Reservation;
+import nl.novi.summerville.springboot.example.security.payload.request.ReservationRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@SpringBootTest
+public class TestReservationController {
+    @Autowired
+    ReservationController reservationController;
+
+    @Test
+    void createReservation(){
+        Reservation reservation = new Reservation();
+        SimpleDateFormat formatter= new SimpleDateFormat("dd-mm-yyyy");
+        reservation.setReservationDate(formatter.format(new Date()));
+        //reservation.setCategoryName("categorie dienst");//TODO
+        //reservationRequest.getHandymanId();
+
+        reservationController.saveReservation(reservation);
+
+
+
+
+
+
+    }
+}
