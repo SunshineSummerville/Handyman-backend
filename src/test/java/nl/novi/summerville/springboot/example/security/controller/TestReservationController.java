f6e471abcf0fc8a919f20b7912c14470a60a84b9
Sunshine <sunshine.summerville@gmail.com>
2021-12-13 12:35:48 +0100

    Setup tests, add test for getCategories

--- src/test/java/nl/novi/summerville/springboot/example/security/controller/TestReservationController.java
+++ src/test/java/nl/novi/summerville/springboot/example/security/controller/TestReservationController.java
@@ -1,33 +1,47 @@
 package nl.novi.summerville.springboot.example.security.controller;
 
-import nl.novi.summerville.springboot.example.security.domain.Reservation;
-import nl.novi.summerville.springboot.example.security.payload.request.ReservationRequest;
+import nl.novi.summerville.springboot.example.security.NoviExamplesApplication;
+import nl.novi.summerville.springboot.example.security.repository.ReservationRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK,classes = NoviExamplesApplication.class )
+@AutoConfigureMockMvc
+@TestPropertySource(
+        locations = "classpath:application.properties")
 
-@SpringBootTest
 public class TestReservationController {
-    @Autowired
-    ReservationController reservationController;
 
-//    @Test
-//    void createReservation(){
-//        Reservation reservation = new Reservation();
-//        SimpleDateFormat formatter= new SimpleDateFormat("dd-mm-yyyy");
-//        reservation.setReservationDate(formatter.format(new Date()));
-//        //reservation.setCategoryName("categorie dienst");//TODO
-//        //reservationRequest.getHandymanId();
-//
-//        reservationController.saveReservation(reservation);
-//
-//
-//
-//
-//
-//
-//    }
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ReservationRepository repository;
+
+
+    @Test
+    public void getCategories() throws Exception {
+        // arrange
+        String endpoint = "/api/categories";
+        // act
+        mvc.perform(get(endpoint)
+                .contentType(MediaType.APPLICATION_JSON))
+                // assert
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(20)));
+
+
+    }
 }
